7e07fd19c72d2d89a0d4bafbe5a68b5d5e5520e4
alexkang <alexkang12345@outlook.com>
2021-11-03 22:15:45 -0700

    refractored, need to finish sneaker entry GUI and revenue list gui

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -1,7 +1,6 @@
 package ui;
 
 
-import model.ProxyEntry;
 import model.SupportEntry;
 import model.investment.CookGroupPurchaseList;
 import model.investment.ProxyPurchaseList;
@@ -15,7 +14,6 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
 
 
 import static javax.swing.ScrollPaneConstants.*;
@@ -47,63 +45,55 @@ public class GUI extends JPanel implements ListSelectionListener {
 
 
     private void initializeNewPanel(SupportEntryList supportEntryList, String typeOfEntry, JPanel mainPanel) {
-        SupportEntryCellRenderer cellRenderer = new SupportEntryCellRenderer();
         DefaultListModel<SupportEntry> defaultListModel = new DefaultListModel<>();
         //make the default list model defaultListModel into a jlist
         JList<SupportEntry> entryJlist = new JList<>(defaultListModel);
-        //we can see 10 elements
-        entryJlist.setVisibleRowCount(3);
         //show how it is rendered, in this case it is string
-        entryJlist.setCellRenderer(cellRenderer);
-
+        entryJlist.setCellRenderer(new SupportEntryCellRenderer());
         //button
-        JButton addEntryButton = new JButton("add" + typeOfEntry + "entry");
+        JButton addEntryButton = new JButton("add" + " " + typeOfEntry + " " + "entry");
         //how big is it? may use addEntryButton.setbounds(), but if u have manager, no need to do it.
-
         //all the panels
         JPanel entryPanel = new JPanel();
         //but this doesnt work why> entryPanel.add(proxyEntryJlist)
-        entryJlist.setAlignmentX(CENTER_ALIGNMENT);
-
         //adding button to pannel
         entryPanel.add(addEntryButton, Component.BOTTOM_ALIGNMENT);
 
-        entryPanel.setBorder(BorderFactory.createTitledBorder(typeOfEntry + "Entries"));
+        entryPanel.setBorder(BorderFactory.createTitledBorder(typeOfEntry + " " + "Entries"));
 
         entryPanel.setPreferredSize(new Dimension(400, 200));
         mainPanel.add(entryPanel);
         entryPanel.add(entryJlist);
-        entryPanel.add(new JScrollPane(entryJlist,VERTICAL_SCROLLBAR_AS_NEEDED,
-                HORIZONTAL_SCROLLBAR_AS_NEEDED));
+        entryPanel.add(new JScrollPane(entryJlist,VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_AS_NEEDED));
 
         addEntryButton.addActionListener(new ActionListener() {
-            @Override
             public void actionPerformed(ActionEvent e) {
-                String entryName = JOptionPane.showInputDialog(null,
-                        typeOfEntry + "name?",
-                        "Enter" + typeOfEntry + "name",
-                        JOptionPane.QUESTION_MESSAGE);
+                String entryName = JOptionPane.showInputDialog(null, typeOfEntry + " " + "name?",
+                        "Enter" + " " + typeOfEntry + " " + "name", JOptionPane.QUESTION_MESSAGE);
+                if (entryName == null || entryName.length() == 0) {
+                    JOptionPane.showMessageDialog(entryPanel, "please enter a valid name");
+                } else {
+                    addNewEntry(entryName, supportEntryList, defaultListModel);
 
-                double entryPrice = Double.parseDouble(JOptionPane.showInputDialog(null, entryName + "'s price?",
-                        "Enter price",
-                        JOptionPane.QUESTION_MESSAGE));
-
-
-
-                    if (entryName != null) {
-                        SupportEntry supportEntry = new SupportEntry(entryName, entryPrice);
-                        if (supportEntryList.addEntry(supportEntry)) { //either it is true or not, the expression inside
-                            //the if statement is operated --> this means if it is true, one needs to add the new entry
-                            //to the defaultListModel(using addElement), otherwise if it returns false, it means that the
-                            //object whose quantity has  been updated changed the reference object inb the defaultListModel
-                            //default list, and so we do not need to do anything at all
-                            defaultListModel.addElement(supportEntry);
-                        }
-
-                    }
+                }
             }
         });
     }
+
+    private void addNewEntry(String entryName, SupportEntryList supportEntryList,
+                             DefaultListModel<SupportEntry> defaultListModel) {
+        double entryPrice = Double.parseDouble(JOptionPane.showInputDialog(null, entryName + "'s price?",
+                "Enter price", JOptionPane.QUESTION_MESSAGE));
+        SupportEntry supportEntry = new SupportEntry(entryName, entryPrice);
+        if (supportEntryList.addEntry(supportEntry)) {
+            defaultListModel.addElement(supportEntry);
+        }
+    }
+    //either it is true or not, the expression inside
+    //the if statement is operated --> this means if it is true, one needs to add the new entry
+    //to the defaultListModel(using addElement), otherwise if it returns false, it means that the
+    //object whose quantity has  been updated changed the reference object inb the defaultListModel
+    //default list, and so we do not need to do anything at all
 
 
 
