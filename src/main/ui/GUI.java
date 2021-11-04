e7900f2b5d75d8b0842c6786a6c64be28fe44f72
alexkang <alexkang12345@outlook.com>
2021-11-03 20:41:13 -0700

    Working code but need to change number of lines

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -3,7 +3,10 @@ package ui;
 
 import model.ProxyEntry;
 import model.SupportEntry;
+import model.investment.CookGroupPurchaseList;
 import model.investment.ProxyPurchaseList;
+import model.investment.SupportEntryList;
+import model.investment.ThirdPartyCaptchaSolversPurchaseList;
 
 
 import javax.swing.*;
@@ -12,10 +15,15 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+
+
+import static javax.swing.ScrollPaneConstants.*;
 
 public class GUI extends JPanel implements ListSelectionListener {
     private ProxyPurchaseList proxyPurchaseList;
-
+    private CookGroupPurchaseList cookGroupPurchaseList;
+    private ThirdPartyCaptchaSolversPurchaseList thirdPartyCaptchaSolversPurchaseList;
 
 
     @Override
@@ -23,72 +31,79 @@ public class GUI extends JPanel implements ListSelectionListener {
 
     }
 
+
     public GUI() {
-
-        proxyPurchaseList = new ProxyPurchaseList();
-        SupportEntryCellRenderer cellRenderer = new SupportEntryCellRenderer();
-        setLayout(new GridLayout(1, 5));
-        DefaultListModel<ProxyEntry> proxies = new DefaultListModel<>();
-        //make the default list model proxies into a jlist
-
-        JList<ProxyEntry> proxyEntryJlist = new JList<>(proxies);
-        //we can see 10 elements
-        proxyEntryJlist.setVisibleRowCount(3);
-        //show how it is rendered, in this case it is string
-        proxyEntryJlist.setCellRenderer(cellRenderer);
-
-        //button
-        JButton addProxyButton = new JButton("add proxy entry");
-        //how big is it? may use addProxyButton.setbounds(), but if u have manager, no need to do it.
-
-        //all the panels
         JPanel mainPanel = new JPanel(new FlowLayout());
-        JPanel proxyPanel = new JPanel();
-        //but this doesnt work why> proxyPanel.add(proxyEntryJlist)
-        proxyEntryJlist.setAlignmentX(CENTER_ALIGNMENT);
-
-        //adding button to pannel
-        proxyPanel.add(addProxyButton, Component.BOTTOM_ALIGNMENT);
-
-
         add(mainPanel);
         mainPanel.setBorder(BorderFactory.createTitledBorder("Support Entries"));
-        proxyPanel.setBorder(BorderFactory.createTitledBorder("proxy Entries"));
+        proxyPurchaseList = new ProxyPurchaseList();
+        setLayout(new GridLayout(1, 5));
+        initializeNewPanel(proxyPurchaseList, "Proxy", mainPanel);
+        cookGroupPurchaseList = new CookGroupPurchaseList();
+        initializeNewPanel(cookGroupPurchaseList, "Cook group", mainPanel);
+        thirdPartyCaptchaSolversPurchaseList = new ThirdPartyCaptchaSolversPurchaseList();
+        initializeNewPanel(thirdPartyCaptchaSolversPurchaseList, "Third party captcha solver", mainPanel);
+    }
 
-        proxyPanel.setPreferredSize(new Dimension(400, 200));
-        mainPanel.add(proxyPanel);
-        proxyPanel.add(proxyEntryJlist);
-        proxyPanel.add(new JScrollPane(proxyEntryJlist));
 
-        addProxyButton.addActionListener(new ActionListener() {
+    private void initializeNewPanel(SupportEntryList supportEntryList, String typeOfEntry, JPanel mainPanel) {
+        SupportEntryCellRenderer cellRenderer = new SupportEntryCellRenderer();
+        DefaultListModel<SupportEntry> defaultListModel = new DefaultListModel<>();
+        //make the default list model defaultListModel into a jlist
+        JList<SupportEntry> entryJlist = new JList<>(defaultListModel);
+        //we can see 10 elements
+        entryJlist.setVisibleRowCount(3);
+        //show how it is rendered, in this case it is string
+        entryJlist.setCellRenderer(cellRenderer);
+
+        //button
+        JButton addEntryButton = new JButton("add" + typeOfEntry + "entry");
+        //how big is it? may use addEntryButton.setbounds(), but if u have manager, no need to do it.
+
+        //all the panels
+        JPanel entryPanel = new JPanel();
+        //but this doesnt work why> entryPanel.add(proxyEntryJlist)
+        entryJlist.setAlignmentX(CENTER_ALIGNMENT);
+
+        //adding button to pannel
+        entryPanel.add(addEntryButton, Component.BOTTOM_ALIGNMENT);
+
+        entryPanel.setBorder(BorderFactory.createTitledBorder(typeOfEntry + "Entries"));
+
+        entryPanel.setPreferredSize(new Dimension(400, 200));
+        mainPanel.add(entryPanel);
+        entryPanel.add(entryJlist);
+        entryPanel.add(new JScrollPane(entryJlist,VERTICAL_SCROLLBAR_AS_NEEDED,
+                HORIZONTAL_SCROLLBAR_AS_NEEDED));
+
+        addEntryButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String proxyName = JOptionPane.showInputDialog(null,
-                        "Proxy name?",
-                        "Enter proxy name",
+                String entryName = JOptionPane.showInputDialog(null,
+                        typeOfEntry + "name?",
+                        "Enter" + typeOfEntry + "name",
                         JOptionPane.QUESTION_MESSAGE);
 
-                double proxyPrice = Double.parseDouble(JOptionPane.showInputDialog(null, proxyName + "'s price?",
+                double entryPrice = Double.parseDouble(JOptionPane.showInputDialog(null, entryName + "'s price?",
                         "Enter price",
                         JOptionPane.QUESTION_MESSAGE));
 
 
 
-                    if (proxyName != null) {
-                        ProxyEntry proxyEntry = new ProxyEntry(proxyName, proxyPrice);
-                        if (proxyPurchaseList.addEntry(proxyEntry)) { //either it is true or not, the expression inside
+                    if (entryName != null) {
+                        SupportEntry supportEntry = new SupportEntry(entryName, entryPrice);
+                        if (supportEntryList.addEntry(supportEntry)) { //either it is true or not, the expression inside
                             //the if statement is operated --> this means if it is true, one needs to add the new entry
-                            //to the proxies(using addElement), otherwise if it returns false, it means that the
-                            //object whose quantity has  been updated changed the reference object inb the proxies
+                            //to the defaultListModel(using addElement), otherwise if it returns false, it means that the
+                            //object whose quantity has  been updated changed the reference object inb the defaultListModel
                             //default list, and so we do not need to do anything at all
-                            proxies.addElement(proxyEntry);
+                            defaultListModel.addElement(supportEntry);
                         }
 
                     }
             }
         });
     }
-
 
 
 
@@ -101,6 +116,12 @@ public class GUI extends JPanel implements ListSelectionListener {
         //Create and set up the window.
         JFrame frame = new JFrame("Profit or Loss calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JMenuBar menubar = new JMenuBar();
+        JMenuItem item = new JMenuItem("Calculate");
+        item.setOpaque(true);
+        item.setBackground(Color.WHITE);
+        menubar.add(item);
+        frame.setJMenuBar(menubar);
 
         //initalizing the gui and adding it to eh window
         JComponent newContentPane = new GUI();
@@ -110,6 +131,7 @@ public class GUI extends JPanel implements ListSelectionListener {
         //Display the window.
         frame.pack();
         frame.setVisible(true);
+
     }
 
     //This class represents each support entry as its string
